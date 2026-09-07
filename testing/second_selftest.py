27b386c9962dd2c7a19a64edfd71580979b39aa5
David Anuoluwapo <davidanuoluwapo07@gmail.com>
2026-09-07 17:28:27 +0100

    still test 2

---
+++ testing/second_selftest.py
@@ -0,0 +1,9 @@
+name = "pelumi"
+fav_color = "silver"
+school ="lasu"
+age = 16
+birth_year = 2009
+print(f"hi {name}, your favourite color is {fav_color}, and your school choice is {school}. Also you are {age} years old and you are born in {birth_year}.") 
+
+
+
